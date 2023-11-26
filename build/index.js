8466ac9509ef8fee768e788b7b8d381b1c35449a
vishuhanda <itsvishuhanda@gmail.com>
2023-11-26 20:10:33 +0530

    checked rxjs operators

--- build/index.js
+++ build/index.js
@@ -2,10 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.hello = void 0;
 const rxjs_1 = require("rxjs");
+const operators_1 = require("rxjs/operators");
 const emitter = (0, rxjs_1.of)("nameone", "nametwo", "namethree");
 emitter.subscribe((value) => {
     console.log(`name : ${value}`);
 });
+const constemitter = (0, rxjs_1.of)(4, 9, 1, 3, 17);
+const mapValue = constemitter.pipe((0, operators_1.map)((value) => {
+    return Math.sqrt(value);
+}), (0, operators_1.map)((value) => {
+    return `square root: ${value}`;
+}));
 const world = 'world';
 function hello(who = world) {
     return `Hello ${who}! `;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import {of,Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 const emitter: Observable<string> = of("nameone","nametwo","namethree")
-
-
 emitter.subscribe((value:string)=>{
-
     console.log(`name : ${value}`)
-
 })
+
+
+const constemitter = of(4,9,1,3,17)
+const mapValue = constemitter.pipe(
+    map((value: number) => {
+      return Math.sqrt(value)
+    }),
+    map((value: number) => {
+      return `square root: ${value}`
+    })
+  )
+
+
 
 const world = 'world';
 export function hello(who: string = world): string {

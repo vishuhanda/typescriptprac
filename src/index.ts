93ca2019bcd2fb9942c51155497fb4310d62b887
vishuhanda <itsvishuhanda@gmail.com>
2023-12-03 21:14:33 +0530

    practice observables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import {of,Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {of,Observable, async} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+
+
 
 const emitter: Observable<string> = of("nameone","nametwo","namethree")
 emitter.subscribe((value:string)=>{
     console.log(`name : ${value}`)
 })
+
+
+
+
 
 
 const constemitter = of(4,9,1,3,17)
@@ -14,14 +20,40 @@ const mapValue = constemitter.pipe(
     }),
     map((value: number) => {
       return `square root: ${value}`
-    })
+    },
+    catchError((error:any) =>{
+        console.log(`stream caught : ${error}`);
+        return of(null)
+    }))
   )
 
+  mapValue.subscribe((value:string)=>{
+    console.log(`${value}`)
+  })
+
+
+const p_resolve = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+    return resolve("resolve promise")    
+  },5000)
+  
+  
+})
 
 
 const world = 'world';
 export function hello(who: string = world): string {
     return `Hello ${who}! `;
   }
-
 console.log(hello())
+console.log("wow")
+
+const chk=async function handlePromise() {
+  const demo =   await p_resolve
+  console.log(demo)
+  console.log("I am getting resolved before promise resolved")
+}
+
+chk()
+
+
